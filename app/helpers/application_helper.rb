eef2f847ac3ef8e00d2d2776aa12a4fa3b11b5ce
github username <github email address>
2019-10-14 21:25:27 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def list_songs()
+    content_tag(:ul) do
+      @songs.each do |song| 
+        str = song.artist.name + " - " + song.title
+        concat(content_tag(:li, link_to(str, song)))
+      end
+    end
+  end
+
+  def list_artist_in_songs
+
+  end
 end
